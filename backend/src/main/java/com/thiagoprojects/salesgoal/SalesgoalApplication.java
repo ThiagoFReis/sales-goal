0333d43f2475a294ad1abe49937a184fd1109597
ThiagoFReis <thiagofreitas201717@gmail.com>
2022-07-11 10:14:46 -0300

    Project created

---
+++ backend/src/main/java/com/thiagoprojects/salesgoal/SalesgoalApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoprojects.salesgoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesgoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesgoalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thiagoprojects/salesgoal/SalesgoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoprojects.salesgoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesgoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
